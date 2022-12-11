764b242ee09b9220c7f71311c6ff0d86ec355306
Uwe Steinmann <steinm@debian.org>
2022-12-11 15:23:11 +0100

    add missing translations

--- lang.php
+++ lang.php
@@ -3,10 +3,14 @@ $__lang['en_GB'] = array(
 	'paperless_upload_succeded'=>'Upload succeded',
 	'paperless_upload_failed'=>'Upload failed',
 	'paperless_missing_target_folder'=>'Missing target folder',
+	'paperless_no_files_uploaded'=>'No files uploaded',
+	'paperless_upload_maxsize'=>'Max file size exceeded',
 );
 $__lang['de_DE'] = array(
 	'paperless_upload_succeded'=>'Erfolgreich hochgeladen',
 	'paperless_upload_failed'=>'Hochladen fehlgeschlagen',
 	'paperless_missing_target_folder'=>'Zielordner nicht vorhanden',
+	'paperless_no_files_uploaded'=>'Keine Dateien hochgeladen',
+	'paperless_upload_maxsize'=>'Maximale Dateigröße überschritten',
 );
 
