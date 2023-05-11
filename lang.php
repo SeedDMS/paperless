1d9adf9b26bc0a6a5c950296df8c9fefea409510
Uwe Steinmann <steinm@debian.org>
2023-05-11 09:29:59 +0200

    add phrase paperless_jwtsecret_not_set

--- lang.php
+++ lang.php
@@ -6,6 +6,7 @@ $__lang['en_GB'] = array(
 	'paperless_no_files_uploaded'=>'No files uploaded',
 	'paperless_upload_maxsize'=>'Max file size exceeded',
 	'paperless_token_has_expired'=>'Token has expired. Login again before you proceed.',
+	'paperless_jwtsecret_not_set'=>'The secret for the JSON Web Token is not set. This is required for successful login.',
 );
 $__lang['de_DE'] = array(
 	'paperless_upload_succeded'=>'Erfolgreich hochgeladen',
@@ -14,5 +15,6 @@ $__lang['de_DE'] = array(
 	'paperless_no_files_uploaded'=>'Keine Dateien hochgeladen',
 	'paperless_upload_maxsize'=>'Maximale Dateigröße überschritten',
 	'paperless_token_has_expired'=>'Token abgelaufen. Melden Sie sich bitte neu an.',
+	'paperless_jwtsecret_not_set'=>'Das Geheimnis des JSON Web Token ist nicht gesetzt. Dies ist für eine erfolgreiche Anmeldung erforderlich.',
 );
 
