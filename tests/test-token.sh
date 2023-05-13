62d6ab86c5b43d63965175695681aa6db2e2d383
Uwe Steinmann <steinm@debian.org>
2023-05-13 17:05:31 +0200

    login and password can be passed as command line parameters

--- tests/test-token.sh
+++ tests/test-token.sh
@@ -2,7 +2,16 @@
 
 . ./credentials
 
+USERNAME="admin"
+if [ -n "$1" ]; then
+	USERNAME=$1
+fi
+PASSWORD="admin"
+if [ -n "$2" ]; then
+	PASSWORD=$2
+fi
+JSON="{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"}"
 curl --silent -X POST "${URL}/api/token/" \
    -H 'Content-Type: application/json' \
-   -d '{"username":"admin","password":"admin"}' | jq '.'
+   -d $JSON | jq '.'
 
