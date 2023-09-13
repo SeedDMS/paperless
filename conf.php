e9bcdf0d09066ce3395e47da34ee65aa9f357766
Uwe Steinmann <steinm@debian.org>
2023-09-13 08:09:57 +0200

    update release date

--- conf.php
+++ conf.php
@@ -4,7 +4,7 @@ $EXT_CONF['paperless'] = array(
 	'description' => 'This extension adds additional rest api routes to make it behave like a paperless server. Just use the regular paperless apps, .e.g paperless mobile to access SeedDMS.',
 	'disable' => false,
 	'version' => '1.1.3',
-	'releasedate' => '2023-05-30',
+	'releasedate' => '2023-09-13',
 	'author' => array('name'=>'Uwe Steinmann', 'email'=>'uwe@steinmann.cx', 'company'=>'MMK GmbH'),
 	'config' => array(
 		'rootfolder' => array(
